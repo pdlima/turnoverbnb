151309f6d604b1ca337bce91aa25802ec9ae9ae9
pdlima <pedro.lima@noxious.com.br>
2022-05-25 20:54:55 -0300

    validando campos purchase

--- components/CustomInput.vue
+++ components/CustomInput.vue
@@ -1,28 +1,45 @@
 <template>
-  <div class="flex gap-4 text-blue-400">
-    <div
-      class="inline-flex flex-shrink-0 gap-4 border-b border-blue-400 max-w-[280px] w-full"
-    >
-      <slot></slot>
-      <div>
-        <label class="text-sm uppercase" for="amount">{{ label }}</label>
-        <input
-          type="text"
-          class="w-full py-2 bg-transparent"
-          v-model="model"
-          :placeholder="placeholder"
-        />
+  <div>
+    <validation-provider v-slot="{ errors }" :rules="rules">
+      <div class="flex gap-4 text-blue-400">
+        <div
+          class="inline-flex flex-shrink-0 gap-4 border-b border-blue-400 max-w-[280px] w-full"
+        >
+          <slot></slot>
+
+          <div>
+            <label class="text-sm uppercase" for="amount">{{ label }} </label>
+
+            <input
+              type="text"
+              class="w-full py-2 text-black bg-transparent"
+              v-model="model"
+              :placeholder="placeholder"
+            />
+          </div>
+        </div>
+        <CurrencyCode class="self-center w-full" v-show="hasCurrencyCode" />
       </div>
-    </div>
-    <CurrencyCode class="self-center w-full" v-show="hasCurrencyCode" />
+      <span class="mt-2 text-red-400" v-if="errors.length > 0">
+        {{ errors[0] }}
+      </span>
+    </validation-provider>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { ValidationProvider } from "vee-validate";
 
 export default Vue.extend({
+  components: {
+    ValidationProvider,
+  },
   props: {
+    rules: {
+      type: String,
+      required: true,
+    },
     label: {
       type: String,
       required: true,
--- components/PurchaseForm.vue
+++ components/PurchaseForm.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="relative flex flex-col h-full">
+  <validation-observer
+    class="relative flex flex-col h-full"
+    ref="observer"
+    v-slot="{ valid }"
+  >
     <CustomInput
+      rules="required"
       label="Amount"
       :value="form.amount"
       :hasCurrencyCode="true"
       placeholder="40,00"
+      @input="updateAmount"
     >
       <IconCurrency />
     </CustomInput>
 
     <CustomInput
+      rules="required"
       label="Date"
       :value="form.date"
       class="mt-4"
       placeholder="August 8, 2021"
+      @input="updateDate"
     >
       <IconCalendar />
     </CustomInput>
 
     <CustomInput
+      rules="required"
       label="Description"
       :value="form.description"
       class="mt-4"
       placeholder="t-shirt"
+      @input="updateDescription"
     >
       <IconStar />
     </CustomInput>
@@ -30,16 +40,21 @@
     <button
       class="w-full py-2 mt-auto text-white bg-blue-400"
       @click="addPurchase"
+      :disabled="!valid"
     >
       Add Purchase
     </button>
-  </div>
+  </validation-observer>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { ValidationObserver } from "vee-validate";
 
 export default Vue.extend({
+  components: {
+    ValidationObserver,
+  },
   layout: "default",
   name: "Purchase",
   data() {
@@ -52,8 +67,19 @@ export default Vue.extend({
     };
   },
   methods: {
-    addPurchase() {
-      this.$store.dispatch("operations/addPurchase", this.form);
+    updateAmount(input: string) {
+      this.form.amount = input;
+    },
+    updateDate(input: string) {
+      this.form.date = input;
+    },
+    updateDescription(input: string) {
+      this.form.description = input;
+    },
+    async addPurchase() {
+      const isValid = await (this.$refs.observer as any).validate();
+
+      if (isValid) this.$store.dispatch("operations/addPurchase", this.form);
     },
   },
 });
